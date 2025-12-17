016d2096a291a5f87cdd3ead03a74f029d91558a
Kumar, Avik <akumar3076@gatech.edu>
2025-12-17 11:55:42 -0800

    Added README and assets, including gifs and plots

---
+++ README.md
@@ -0,0 +1,36 @@
+# Walker2d Demo + Experimentation with Custom Reward Policies
+
+## Objective/Goals
+* Learn to train an RL policy on a humanoid-like figure, while working with some preexisting realistic phsyics engine
+* Develop understanding of RL and reward policies
+* Deliver learning curves as well as video outputs of trained model
+
+## Approach
+Due to CPU limitations, I decided to use a relatively lightweight simulator, and decided on MuJoCo wrapped with gynasium in python. I then proceeded to train a `walker2d-v4`, one of the many available MuJoCo humanoids. First, I trained it with the default reward policy, and upon viewing the learning curve and videos, I realized that the model was falling too soon with the default policy. Thus, I implemented a custom policy that rewards *stability* and *uprightedness* as well, and not just speed, since this helps the model stabilize and develop more long-term movements. The output videos were longer, reflecting more successful models that could last longer without falling.
+
+## Project Structure
+walker2d-demo/
+├── README.md
+├── requirements.txt
+├── train_walker2d.py
+├── record_video.py
+├── assets/
+|   ├── default_policy_video.gif
+|   └── custom_policy_video.gif
+└── plots/
+    ├── learning_curve_default.csv
+    ├── learning_curve_default.png
+    └── plot_learning_curves.ipynb
+
+## Results
+### Default Policy - Learning Curve
+![Default Policy Learning Curve](plots/learning_curve_default.png)
+
+### Default Policy - Model GIF
+![Default Policy GIF](assets\ppo_walker2d-episode-0.gif)
+
+### Custom Policy - Learning Curve
+![Custom Policy Learning Curve](plots/learning_curve_default.png)
+
+### Custom Policy - Model GIF
+![Custom Policy Model GIF](assets\ppo_walker2d-episode-0.gif)
--- record_video.py
+++ record_video.py
@@ -5,42 +5,38 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
 from gymnasium.wrappers import RecordVideo, TimeLimit
 
-def record_video():
-    # Environment setup (with video)
-    def make_env():
-        env = gym.make("Walker2d-v4", render_mode="rgb_array")
-        env = TimeLimit(env, max_episode_steps=3000)
+# Environment setup (with video)
+def make_env():
+    env = gym.make("Walker2d-v4", render_mode="rgb_array")
+    env = TimeLimit(env, max_episode_steps=3000)
 
-        env = RecordVideo(
-            env,
-            video_folder="./videos/",
-            episode_trigger=lambda episode_id: True,  # record first episode
-            name_prefix="ppo_walker2d"
-        )
-        return env
+    env = RecordVideo(
+        env,
+        video_folder="./videos/",
+        episode_trigger=lambda episode_id: True,  # record first episode
+        name_prefix="ppo_walker2d"
+    )
+    return env
 
-    env = DummyVecEnv([make_env])
-
-
-    # Get stats from pickle
-    env = VecNormalize.load("vecnormalize.pkl", env)
-    env.training = False
-    env.norm_reward = False
+env = DummyVecEnv([make_env])
 
 
-    # Load model from zip folder
-    model = PPO.load("ppo_walker2d_final", env=env, device="cpu")
+# Get stats from pickle
+env = VecNormalize.load("vecnormalize.pkl", env)
+env.training = False
+env.norm_reward = False
 
 
-    # Run episode
-    obs = env.reset()
-    done = False
+# Load model from zip folder
+model = PPO.load("ppo_walker2d_final", env=env, device="cpu")
 
-    while not done:
-        action, _ = model.predict(obs, deterministic=True)
-        obs, reward, done, info = env.step(action)
 
-    env.close()
+# Run episode
+obs = env.reset()
+done = False
 
-if __name__ == "__main__":
-    record_video()
+while not done:
+    action, _ = model.predict(obs, deterministic=True)
+    obs, reward, done, info = env.step(action)
+
+env.close()
